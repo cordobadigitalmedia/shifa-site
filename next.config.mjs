09bc8d47ef3e9e4fda210ac4a566382f80d4a985
ismael <ismael@cordobadigitalmedia.com>
2024-11-30 13:50:00 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,58 @@ const nextConfig = {
       },
     ]
   },
+  async redirects() {
+    return [
+      {
+        source: "/community-workshops",
+        destination: "/resources/workshop",
+      },
+      {
+        source: "/training",
+        destination: "/resources/training",
+      },
+      {
+        source: "/shifa-lessons",
+        destination: "/resources/lesson",
+      },
+      {
+        source: "/advice-faq",
+        destination: "/qa",
+      },
+      {
+        source: "/contact",
+        destination: "/forms/contact-form",
+      },
+      {
+        source: "/counseling-request-form",
+        destination: "/forms/counseling-request-form",
+      },
+      {
+        source: "/improving-communication",
+        destination: "/qa",
+      },
+      {
+        source: "/ramadan",
+        destination: "/qa",
+      },
+      {
+        source: "/youth",
+        destination: "/qa",
+      },
+      {
+        source: "/raising-children",
+        destination: "/qa",
+      },
+      {
+        source: "/disorders",
+        destination: "/qa",
+      },
+      {
+        source: "/schizophrenia",
+        destination: "/qa",
+      },
+    ]
+  },
 }
 
 export default nextConfig
