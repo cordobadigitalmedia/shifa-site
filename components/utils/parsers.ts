5a0a4d6fbf7bd860b54cf566687c921ae5a62676
ismael <ismael@cordobadigitalmedia.com>
2024-12-04 18:50:52 -0700

    remove table from email format but add 2 spaces

--- components/utils/parsers.ts
+++ components/utils/parsers.ts
@@ -10,3 +10,30 @@ export function isAbsoluteLink(url: string): boolean {
     return false
   }
 }
+
+export function createHtmlTable(data: [string, FormDataEntryValue][]) {
+  const tableRows = data
+    .map(
+      ([key, value]) => `
+      <tr>
+        <td style="border: 1px solid #ddd; padding: 8px; text-align: left;"><b>${key}</b></td>
+        <td style="border: 1px solid #ddd; padding: 8px; text-align: left;">${value}</td>
+      </tr>
+    `
+    )
+    .join("")
+
+  return `
+    <table style="border-collapse: collapse; width: 100%; max-width: 600px;">
+      <thead>
+        <tr>
+          <th style="border: 1px solid #ddd; padding: 12px; text-align: left; background-color: #f2f2f2;">Field</th>
+          <th style="border: 1px solid #ddd; padding: 12px; text-align: left; background-color: #f2f2f2;">Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${tableRows}
+      </tbody>
+    </table>
+  `
+}
